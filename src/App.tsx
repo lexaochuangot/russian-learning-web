c42d437eb337880b9f1a17e97d1c6050d216641c
Le Uyen <uyenlee.152@gmail.com>
2025-08-21 19:39:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Study from "./pages/Study";
-import Stats from "./pages/Stats";
+import QuizPage from "./pages/QuizPage";
 import Settings from "./pages/Settings";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/study" element={<Study />} />
+            <Route path="/quiz" element={<QuizPage />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
