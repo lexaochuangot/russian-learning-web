7fdf31a2269099e8af1821ace7dde0daa9162c0b
Le Uyen <uyenlee.152@gmail.com>
2025-08-21 16:59:22 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-indigo-600 text-white px-6 py-3 shadow-md">
+      <ul className="flex space-x-6">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/study">Study</Link></li>
+        <li><Link to="/quiz">Quiz</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
