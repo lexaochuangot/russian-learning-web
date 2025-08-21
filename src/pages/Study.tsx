34e53af6580a3388a159a6d78ccc6733042746a5
Le Uyen <uyenlee.152@gmail.com>
2025-08-21 17:00:22 +0700

    Update Study.tsx

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -1,1 +1,29 @@
+import { useState } from "react";
+import Card from "../components/Card";
+import sampleDeck from "../data/sampleDeck.json";
+
+export default function Study() {
+  const [index, setIndex] = useState(0);
+  const deck = sampleDeck[0].words;
+
+  return (
+    <div className="flex flex-col items-center space-y-6">
+      <Card front={deck[index].ru} back={deck[index].en} />
+      <div className="flex space-x-4">
+        <button
+          className="px-4 py-2 bg-gray-300 rounded-lg"
+          onClick={() => setIndex((i) => (i > 0 ? i - 1 : deck.length - 1))}
+        >
+          Prev
+        </button>
+        <button
+          className="px-4 py-2 bg-gray-300 rounded-lg"
+          onClick={() => setIndex((i) => (i + 1) % deck.length)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
 
